5914bc4bece86ba2f3f23a95e7827d766c11094d
tsundori25 <tsundori25@gmail.comgit config --global user.email tsundori25@gmail.com>
2022-04-07 21:04:57 +0700

    ADD:slider

--- src/components.js/Home.js
+++ src/components.js/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
 }
 
 export default Home;
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  overflow-x: hidden;
+  }
+`;
